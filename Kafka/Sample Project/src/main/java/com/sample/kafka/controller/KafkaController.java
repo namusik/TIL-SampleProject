cda1e8f7d08111cfc1be3929b1ba0992aa2f9550
woosiknam <wsnam0418@gmail.com>
2022-02-10 19:45:18 +0900

    Kafka setting

---
+++ Kafka/Sample Project/src/main/java/com/sample/kafka/controller/KafkaController.java
@@ -0,0 +1,22 @@
+package com.sample.kafka.controller;
+
+import com.sample.kafka.model.Chatmessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import com.sample.kafka.service.KafkaProducerService;
+
+@RestController
+@RequiredArgsConstructor
+public class KafkaController {
+    private final KafkaProducerService producerService;
+
+    @PostMapping("/kafka")
+    public String sendMessage(@RequestBody Chatmessage chatmessage) {
+        producerService.sendMessage(chatmessage);
+
+        return "success";
+    }
+}
---
+++ Kafka/Sample Project/src/main/java/com/sample/kafka/service/KafkaProducerService.java
@@ -0,0 +1,18 @@
+package com.sample.kafka.service;
+
+import com.sample.kafka.model.Chatmessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class KafkaProducerService {
+    private static final String TOPIC = "wowowo";
+    private final KafkaTemplate<String, Chatmessage> kafkaTemplate;
+
+    public void sendMessage(Chatmessage chatmessage) {
+        System.out.println("chatmessage = " + chatmessage.getContext());
+        kafkaTemplate.send(TOPIC, chatmessage);
+    }
+}
