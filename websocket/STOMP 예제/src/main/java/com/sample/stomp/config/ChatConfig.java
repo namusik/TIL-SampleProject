2ff376502df0ac3321af2b58b6ab7f61ec253608
woosiknam <wsnam0418@gmail.com>
2022-02-17 15:16:42 +0900

    STOMP 채팅방 VIEW 수정

--- websocket/STOMP 예제/src/main/java/com/sample/stomp/config/ChatConfig.java
+++ websocket/STOMP 예제/src/main/java/com/sample/stomp/config/ChatConfig.java
@@ -2,10 +2,12 @@ package com.sample.stomp.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
+@EnableWebSocketMessageBroker
 public class ChatConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
