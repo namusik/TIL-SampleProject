77ef2b9c2d626c400cb0fc82033b2b9bf32272d7
woosiknam <wsnam0418@gmail.com>
2022-02-18 12:22:56 +0900

    어노테이션 정리

--- websocket/STOMP 예제/src/main/java/com/sample/stomp/model/ChatMessage.java
+++ websocket/STOMP 예제/src/main/java/com/sample/stomp/model/ChatMessage.java
@@ -15,7 +15,10 @@ public class ChatMessage {
     }
 
     private MessageType type;
+    //채팅방 ID
     private String roomId;
+    //보내는 사람
     private String sender;
+    //내용
     private String message;
 }
