0319fd8ba1d0813fa227347be448c934734c676a
Woosik Nam <79183229+namusik@users.noreply.github.com>
2022-04-07 18:37:40 +0900

    수정

--- AWS/Amazon SES/AWS SES 실습예제/src/main/java/com/example/aws_ses/controller/SendEmailController.java
+++ AWS/Amazon SES/AWS SES 실습예제/src/main/java/com/example/aws_ses/controller/SendEmailController.java
@@ -1,6 +1,5 @@
 package com.example.aws_ses.controller;
 
-import com.example.aws_ses.security.Aes;
 import com.example.aws_ses.service.SendEmailService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,10 +24,6 @@ public class SendEmailController {
         receivers.add(email);
         //제목
         String subject = "SES Test";
-
-        //링크를 보낼 때, email 부분을 문자열로 바꿔줘야함. 노출이 안되도록. Aes의 방식을 사용하니 암호문자에 '/'가 포함되서 인식오류
-//        Aes aes = new Aes();
-//        String enc = aes.encrypt(email);
 
         //패스워드 입력할 수 있는 API URI를 본문에 담아서 보내줌.
         String content = "http://localhost:8080/user/resetpw/"+email;
