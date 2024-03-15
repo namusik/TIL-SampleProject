ac85e2f9a5d84c30565d8885501de2eac8efce15
namusik <79183229+namusik@users.noreply.github.com>
2024-03-15 18:23:21 +0900

    til

--- Java/Java Sample Code/tcp/src/main/java/com/example/tcp/client/TcpServer.java
+++ Java/Java Sample Code/tcp/src/main/java/com/example/tcp/client/TcpServer.java
@@ -4,14 +4,18 @@ import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketTimeoutException;
 
 @Slf4j
 @Service
 public class TcpServer {
     private static final int PORT = 8080;
+    private static final int TIMEOUT = 10000;
 
     @PostConstruct
     // 객체의 초기화가 완료된 후 자동으로 호출. TCP 서버를 시작
@@ -21,17 +25,26 @@ public class TcpServer {
             // 서버가 클라이언트의 연결을 비동기적으로 수신할 수 있도록. 서버의 main thread가 blocking 되지 않고 다른 작업을 처리할 수 있도록
             try {
                 ServerSocket serverSocket = new ServerSocket(PORT);
-                log.info("TCP Server start : {}", PORT);
 
-                while (true) {
-                    Socket clientSocket = serverSocket.accept();
+                // 클라이언트 연결 요청 10초 기다림
+                serverSocket.setSoTimeout(TIMEOUT);
 
+                System.out.println("Server is listening on port " + PORT);
+                System.out.println("Waiting for a connection for 10 seconds...");
+
+                try {
+                    Socket socket = serverSocket.accept();
+
+                    log.info("소켓 연결 성공 : {}", socket.getRemoteSocketAddress());
+
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                } catch (SocketTimeoutException e) {
+                    log.error("SocketTimeoutException 발생", e);
                 }
-
             } catch (IOException e) {
+                log.error("서버 에러 발생", e);
                 throw new RuntimeException(e);
             }
         }).start();
-
     }
 }
