57ea5c24626ae6bc81c82cb4c2de381103c03df3
namusik <79183229+namusik@users.noreply.github.com>
2024-04-19 11:35:22 +0900

    til

---
+++ Java/Java Sample Code/samplecode/src/main/java/org/example/samplecode/dataType/string/TemplateCode.java
@@ -0,0 +1,10 @@
+package org.example.samplecode.dataType.string;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TemplateCode {
+    public static void main(String[] args) {
+
+    }
+}
