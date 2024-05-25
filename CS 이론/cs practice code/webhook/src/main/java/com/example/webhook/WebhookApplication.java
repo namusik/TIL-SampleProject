3ad0f78e0888f35e0b9d115fcce275b2706ca3ee
woosiknam <wsnam0418@gmail.com>
2024-05-25 12:16:12 +0900

    til

---
+++ CS 이론/cs practice code/webhook/src/main/java/com/example/webhook/WebhookApplication.java
@@ -0,0 +1,13 @@
+package com.example.webhook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebhookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebhookApplication.class, args);
+    }
+
+}
---
+++ CS 이론/cs practice code/webhook/src/test/java/com/example/webhook/WebhookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webhook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebhookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
