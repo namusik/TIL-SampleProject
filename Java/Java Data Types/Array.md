3dcc4c8084007fa318c0570c03273f4d0612322c
namusik <79183229+namusik@users.noreply.github.com>
2024-06-25 19:41:26 +0900

    TIL

---
+++ Java/Java Data Types/Array.md
@@ -0,0 +1,19 @@
+# Java Array 
+
+
+## Arrays.stream()
+
+```java
+String[] anArray = new String[] {"Milk", "Tomato", "Chips"};
+Stream<String> aStream = Arrays.stream(anArray);
+```
+
+- 배열을 stream으로 변환할 때 사용
+
+
+
+##  출처
+
+https://www.baeldung.com/java-arrays-guide
+
+https://www.baeldung.com/java-stream-to-array
--- Java/Java Practice Code/basic/src/main/java/org/practice/basic/dataType/array/ArrayToStream.java
+++ Java/Java Practice Code/basic/src/main/java/org/practice/basic/dataType/array/ArrayToStream.java
@@ -1,13 +1,24 @@
 package org.practice.basic.dataType.array;
 
 import java.util.Arrays;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
-public class ArrayExample {
+public class ArrayToStream {
+
     public static void main(String[] args) {
+
         String[] anArray = new String[] {"Milk", "Tomato", "Chips"};
         Stream<String> aStream = Arrays.stream(anArray);
 
-        System.out.println("aStream = " + aStream);
+        aStream.forEach(System.out::println);
+
+
+        int[] ints = new int[] {1,2,3,4};
+
+        IntStream stream = Arrays.stream(ints);
+
+        System.out.println("stream = " + stream);
+
     }
 }
